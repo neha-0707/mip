9d8b0097ca2398307c937181f913de5a654e5f51
unknown <vineetabhujle26@gmail.com>
2021-03-08 23:56:03 +0530

    about us component

--- VirtualStocksAngular/src/app/app-routing.module.ts
+++ VirtualStocksAngular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: 'leaderboard',component:LeaderboardComponent},{
+path: 'aboutus',component: AboutusComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ VirtualStocksAngular/src/app/app.module_BACKUP_1389.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
+import { SharedService } from './shared.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MatListModule } from '@angular/material/list';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AboutusComponent } from './aboutus/aboutus.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeaderboardComponent,
+    AboutusComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+  ],
+  providers: [SharedService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
