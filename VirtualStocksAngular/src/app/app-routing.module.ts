4f69da9807aa2c60ed3c44a0f3897f6469137cd7
neha-0707 <patil.nv@somaiya.edu>
2021-03-08 21:03:47 +0530

    idr

--- VirtualStocksAngular/src/app/app-routing.module.ts
+++ VirtualStocksAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'leaderboard',component:LeaderboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
