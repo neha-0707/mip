9dd43eef84f85eca1acd218a04837c7506545eed
cmd-dev <tanmay.armal@somaiya.edu>
2021-03-23 16:10:01 +0530

    changed user model

--- VirtualStocksDjango/UserApp/models.py
+++ VirtualStocksDjango/UserApp/models.py
@@ -6,7 +6,8 @@ from django.db import models
 #     UserPassword = models.CharField(max_length=50)
 
 class User(models.Model):
-    UserID = models.CharField(primary_key=True, max_length=15)
+    UserID = models.IntegerField(primary_key=True)
+    UserName = models.CharField(max_length=15)
     UserMoney = models.DecimalField( blank=False, null=False, decimal_places=2, max_digits=10)
     UserPassword = models.CharField(blank=False, null=False, max_length=50)
-    PortfolioID=models.ForeignKey('Portfolio.Portfolio')
+    # PortfolioID=models.ForeignKey('Portfolio.Portfolio',on_delete=models.CASCADE))
