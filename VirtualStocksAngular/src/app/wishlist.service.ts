8e10714612a1cb645a34150e556692edcb3599ab
unknown <vineetabhujle26@gmail.com>
2021-04-03 00:46:06 +0530

    searchwishlist

--- VirtualStocksAngular/src/app/wishlist.service.ts
+++ VirtualStocksAngular/src/app/wishlist.service.ts
@@ -1,17 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
-export class SendstockService {
+export class WishlistService {
+
   readonly APIUrl = " http://127.0.0.1:8000/api/";
 
   constructor(private http: HttpClient) { }
 
-  stockname(stock:string):Observable<any[]>{
-    return this.http.get<any[]>(this.APIUrl+'stock/'+stock);
+  addtowishlist(stock:string):Observable<any[]>{
+    return this.http.post<any[]>(this.APIUrl,stock);
   }
-
-
 }
