4acb8c82f5983e2eb85e5dbd622c476801635646
neha-0707 <patil.nv@somaiya.edu>
2021-04-10 23:50:59 +0530

    portfolio done fr

--- VirtualStocksAngular/src/app/portfolio/portfolio.component.ts
+++ VirtualStocksAngular/src/app/portfolio/portfolio.component.ts
@@ -1,78 +1,102 @@
-import {Component, OnInit} from '@angular/core';
-import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { SharedService } from '../shared.service';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-portfolio',
   styleUrls: ['./portfolio.component.scss'],
-  templateUrl: './portfolio.component.html', 
+  templateUrl: './portfolio.component.html',
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
 export class PortfolioComponent implements OnInit {
-  Response={};
-  initial:any;
-  curr:any;
-  stocks:any;
-  dataSource = ELEMENT_DATA;
+  b = {
+    averagePrice: null,
+    high: null,
+    low: null,
+    previous_close: null,
+    symbol: null,
+    change: null,
+    company_name: null,
+    number: null
+  };
+  Response = {};
+  initial: any;
+  curr: any;
+  stocks: any;
+  dataSource: any;
   columnsToDisplay = ['symbol', 'change', 'number'];
   expandedElement: PeriodicElement | null;
+  
   constructor(private service: SharedService) { }
 
   ngOnInit(): void {
     this.getPortfolio();
   }
-  getPortfolio(){
-    this.service.getPortfolio().subscribe(res=>{
+  getPortfolio() {
+    this.service.getPortfolio().subscribe(res => {
       console.log(res);
+      this.dataSource=null;
       this.Response = res;
       this.splitData();
+      this.dataSource =  ELEMENT_DATA;
     },
-    err=>{console.log(err);
-    })  
+      err => {
+        console.log(err);
+      })
   }
-  splitData(){
-    this.initial= (this.Response as any).UnrealizedValueInitial;
-    this.curr=(this.Response as any).UnrealizedValueCurr;
-    this.stocks=(this.Response as any).stocks;
-    /*for(var i in this.stocks){
-      ELEMENT_DATA[i].averagePrice=(this.stocks[i] as any).stock.averagePrice;
-      ELEMENT_DATA[i].high=(this.stocks[i] as any).stock.high;
-      ELEMENT_DATA[i].low=(this.stocks[i] as any).stock.low;
-      ELEMENT_DATA[i].previous_close=(this.stocks[i] as any).stock.previous_close;
-      ELEMENT_DATA[i].symbol=(this.stocks[i] as any).stock.symbol;
-      ELEMENT_DATA[i].change=(this.stocks[i] as any).stock.change;
-      ELEMENT_DATA[i].company_name=(this.stocks[i] as any).stock.company_name;
-      ELEMENT_DATA[i].number = ((this.stocks[i] as any).number);
-    }*/
-    for(var i in this.stocks){
-      ELEMENT_DATA[i]=this.stocks.stock;
+  splitData() {
+    ELEMENT_DATA=[];
+    this.initial = (this.Response as any).UnrealizedValueInitial;
+    this.curr = (this.Response as any).UnrealizedValueCurr;
+    this.stocks = (this.Response as any).stocks;
+
+
+    for (var i in this.stocks) {
+      this.b.averagePrice = this.stocks[i]["stock"]["averagePrice"];
+      this.b.high = this.stocks[i]["stock"]["high"];
+      this.b.low = this.stocks[i]["stock"]["low"];
+      this.b.previous_close = this.stocks[i]["stock"]["previous_close"];
+      this.b.symbol = this.stocks[i]["stock"]["symbol"];
+      this.b.change = this.stocks[i]["stock"]["change"];
+      this.b.company_name = this.stocks[i]["stock"]["company_name"];
+      this.b.number = this.stocks[i].number;
+      ELEMENT_DATA.push(this.b);
+      this.b = {
+        averagePrice: null,
+        high: null,
+        low: null,
+        previous_close: null,
+        symbol: null,
+        change: null,
+        company_name: null,
+        number: null
+      };
     }
-    console.log(this.initial);
-    console.log(this.curr);
-    console.log(this.stocks);
-    console.log(ELEMENT_DATA);
+
+
   }
 }
 
 export interface PeriodicElement {
   averagePrice: number,
-        high: number,
-        low: number,
-        previous_close: number,
-        symbol: string,
-        change: string,
-        company_name: string,
-        number: number
+  high: number,
+  low: number,
+  previous_close: number,
+  symbol: string,
+  change: string,
+  company_name: string,
+  number: number
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  ];
- 
+let ELEMENT_DATA: PeriodicElement[] = [
+];
 
-  
+
+
